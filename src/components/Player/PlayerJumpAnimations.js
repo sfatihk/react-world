27f6b8c4181c5a275f942dcfcf2758077802342c
sfatihk <sakkarmen@gmail.com>
2020-11-22 19:27:54 +0300

    Update jump animation for mario mini

--- src/components/Player/PlayerJumpAnimations.js
+++ src/components/Player/PlayerJumpAnimations.js
@@ -22,9 +22,9 @@ const animations = {
 
   //mario jumps
 
-  "18000-18500": { startHeight: 200, endHeight: 100, type: "fall" },
-  "18500-18580": { startHeight: 100, endHeight: 200, type: "jump" },
-  "18580-18770": { startHeight: 200, endHeight: 100, type: "fall" },
+  "18000-18500": { startHeight: 270, endHeight: 100, type: "fall" },
+  "18500-18580": { startHeight: 100, endHeight: 270, type: "jump" },
+  "18580-18770": { startHeight: 270, endHeight: 100, type: "fall" },
 
   "18900-19070": { startHeight: 347, endHeight: 100, type: "fall" },
   "19100-19350": { startHeight: 100, endHeight: 347, type: "jump" },
@@ -42,19 +42,19 @@ const animations = {
   //end jumps
   "28100-28850": { startHeight: 300, endHeight: 100, type: "fall" },
   "28900-29150": { startHeight: 100, endHeight: 10000, type: "jump" },
-  "29150-30000": { startHeight: 300, endHeight: 100, type: "fall" }
+  "29150-30000": { startHeight: 300, endHeight: 100, type: "fall" },
 };
 
 let lastAnimType = "fall";
 let lastKeyframes = [100, 0, 100];
 
-const jump = props => keyframes`
+const jump = (props) => keyframes`
   0%  {bottom: ${lastKeyframes[0] + "px"}}
   35%  {bottom: ${lastKeyframes[1] + "px"}}
   100% {bottom: ${lastKeyframes[2] + "px"}}
 `;
 
-const freeFall = props => keyframes`
+const freeFall = (props) => keyframes`
   0%  {bottom: ${lastKeyframes[0] + "px"}}
   100% {bottom: ${lastKeyframes[2] + "px"}}
 `;
@@ -63,18 +63,18 @@ const jumpNow = css`
   ${jump} .3s forwards ease-out;
 `;
 
-const fallNow = props => css`
+const fallNow = (props) => css`
   ${freeFall} .1s forwards ease-out;
 `;
 
 /** search in `animations` by frame(scroll) */
-const determineAnim = scroll => {
+const determineAnim = (scroll) => {
   let anim = null;
 
   const currFrame = scroll;
 
   let animRange;
-  Object.keys(animations).some(k => {
+  Object.keys(animations).some((k) => {
     animRange = k.split("-");
 
     //if it out of current range
@@ -93,7 +93,7 @@ const determineAnim = scroll => {
   lastKeyframes = [
     anim.startHeight,
     (anim.endHeight - anim.startHeight) / 2 + anim.endHeight,
-    anim.endHeight
+    anim.endHeight,
   ];
 
   lastAnimType = anim.type;
@@ -103,7 +103,7 @@ const determineAnim = scroll => {
  *
  * @param {*} scroll
  */
-const getJumpAnimation = scroll => {
+const getJumpAnimation = (scroll) => {
   determineAnim(scroll);
 
   return lastAnimType === "jump" ? jumpNow : fallNow;
